284c39555d7678edf00d923d85edca0a127c3756
zen <atthezenith@proton.me>
2025-03-15 20:24:48 +0300

    Release v2.0.0 fixed

---
+++ Game Modules/8444591321 Bedwars Mega Match.lua
@@ -0,0 +1,49 @@
+local LunarVape = _G.LunarVape
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and LunarVape then 
+		warn(err)
+    LunarVape:CreateNotification('Lunar Vape', 'Failed to load : '..err, 30, 'Alert') 
+	end
+	return res
+end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function() 
+		return readfile(file) 
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local function downloadFile(path, func)
+	if not isfile(path) and not _G.LunarVapeDeveloper then
+		local suc, res = pcall(function() 
+			return game:HttpGet('https://raw.githubusercontent.com/AtTheZenith/LunarVape/'..readfile('Lunar Vape/Profiles/Commit.txt')..'/'..select(1, path:gsub('Lunar Vape/', '')), true) 
+		end)
+		if res == '404: Not Found' then
+			warn(string.format('Error while downloading file %s: %s', path, res)); return
+		elseif not suc then
+			error(string.format('Error while downloading file %s: %s', path, res)); return
+		end
+		if path:find('.lua') then 
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after LunarVape updates.\n'..res 
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+
+LunarVape.Place = 6872274481
+local GAME_REGISTRY = loadstring(downloadFile('Lunar Vape/Game Modules/Registry.lua'))()
+local GAME_NAME = GAME_REGISTRY[tostring(LunarVape.Place)] and ' ' .. GAME_REGISTRY[tostring(LunarVape.Place)] or ''
+
+if isfile('Lunar Vape/Game modules/'..LunarVape.Place..GAME_NAME..'.lua') then
+	loadstring(readfile('Lunar Vape/Game modules/'..LunarVape.Place..GAME_NAME..'.lua'), 'Lunar Vape/Game modules/'..LunarVape.Place..GAME_NAME..'.lua')()
+else
+	if not _G.LunarVapeDeveloper then
+		local suc, res = pcall(function() 
+			return game:HttpGet('https://raw.githubusercontent.com/AtTheZenith/LunarVape/'..readfile('Lunar Vape/Profiles/Commit.txt')..'/Game modules/'..LunarVape.Place..GAME_NAME..'.lua', true) 
+		end)
+		if suc and res ~= '404: Not Found' then
+			loadstring(readfile('Lunar Vape/Game modules/'..LunarVape.Place..GAME_NAME..'.lua'), 'Lunar Vape/Game modules/'..LunarVape.Place..GAME_NAME..'.lua')()
+		end
+	end
+end
